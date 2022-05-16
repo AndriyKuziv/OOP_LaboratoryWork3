b986c4198ce9a7e258084d98506223d5a368a3fc
AndriyKuziv <61407998+AndriyKuziv@users.noreply.github.com>
2022-05-16 10:39:03 +0300

    Add files via upload

---
+++ HeroesDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    public interface IHeroesDAL
+    {
+        List<Heroes> SelectAllHeroes();
+        void AddHero(string n, int h, int dps);
+    }
+
+    public class HeroesDAL : IHeroesDAL
+    {
+        List<Heroes> heroesList = new List<Heroes>();
+
+        public void AddHero(string n, int h, int dps)
+        {
+            heroesList.Add(new Heroes() { Name = n, HP = h, DPS = dps});
+        }
+
+        public List<Heroes> SelectAllHeroes()
+        {
+            //heroesList.Add(new Heroes() { Name = "Sanya", HP = 100, DPS = 200 });
+            //heroesList.Add(new Heroes() { Name = "Petya", HP = 500, DPS = 10 });
+            //heroesList.Add(new Heroes() { Name = "Taras", HP = 1000, DPS = 2000 });
+
+            return heroesList;
+        }
+    }
+}
